61e69d82f38535a77176d384a140550f6d4bfa74
Dragan Cajic <cajic_dragan@yahoo.com>
2021-06-16 13:08:13 +0200

    always start with RED (BAR)!

--- src/org/eu5/learn_pisio/isbntools/ValidateISBN.java
+++ src/org/eu5/learn_pisio/isbntools/ValidateISBN.java
@@ -1,4 +1,20 @@
 package org.eu5.learn_pisio.isbntools;
 
 public class ValidateISBN {
+
+	// Step 3: Refactor (i -> isbn)
+	public boolean checkISBN(int isbn) {
+		
+		if (isbn == 140449116) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		// Step 1: Getting to red
+		//return false;
+		
+		// Step 2: Getting to green
+		//return true;
+	}
 }
--- test/org/eu5/learn_pisio/isbntools/ValidateISBNTest.java
+++ test/org/eu5/learn_pisio/isbntools/ValidateISBNTest.java
@@ -6,9 +6,29 @@ import org.junit.Test;
 
 public class ValidateISBNTest {
 
+	/*
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		//fail("This test needs to fail!");
+	}
+	*/
+	
+	//int a = 2_100_000_006; // check range for integer
+	
+	@Test
+	public void checkAValidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void checkAnIvalidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449117);
+		//fail();
+		assertFalse(result);
 	}
 
 }
