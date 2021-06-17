e588716d292e657dc72caa0962a2cbb5f4c49889
Dragan Cajic <cajic_dragan@yahoo.com>
2021-06-17 23:25:01 +0200

    get red bar first!

--- test/org/eu5/learn_pisio/isbntools/StockManagementTests.java
+++ test/org/eu5/learn_pisio/isbntools/StockManagementTests.java
@@ -1,10 +1,17 @@
 package org.eu5.learn_pisio.isbntools;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class StockManagementTests {
+
+	@Before
+	public void setup() {
+	
+	}
 	
 	@Test
 	public void testCanGetACorrectLocatorCode() {
@@ -32,5 +39,15 @@ public class StockManagementTests {
 		String isbn = "0140177396";
 		String locatorCode = stockManager.getLocatorCode(isbn);
 		assertEquals("7396J4", locatorCode);
+	}
+	
+	@Test
+	public void databaseIsUsedIfDataIsPresent() {
+		fail();
+	}
+	
+	@Test
+	public void webServiceIsUsedIfDataIsNotPresentInDatabase() {
+		fail();
 	}
 }
